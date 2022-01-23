d0ac704de7ebfb57035a0b72a4d6568a8c7fba80
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:02:35 +0800

    1010

---
+++ Chapter2/Practice/1010.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double a, b, ans;
+
+    cin >> a >> b;
+    ans = a / b * 1.0;
+
+    cout << fixed << setprecision(9) << ans << endl;
+    return 0;
+}
