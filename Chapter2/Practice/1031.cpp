e72c7bf0029e7207f0dc7479011f564cc129322d
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:40:36 +0800

    1031

---
+++ Chapter2/Practice/1031.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b, c;
+
+    cin >> n;
+    a = n / 100;
+    b = (n / 10) % 10;
+    c = n % 10;
+
+    cout << c << b << a << endl;
+    return 0;
+}
