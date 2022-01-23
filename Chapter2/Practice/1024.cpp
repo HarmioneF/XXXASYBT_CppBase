fbc15a03da1b6c810be1888631704c86a29e09db
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:28:18 +0800

    1024

---
+++ Chapter2/Practice/1024.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double a;
+
+    cin >> a;
+    cout << fixed << setprecision(3) << a << endl;
+
+    return 0;
+}
