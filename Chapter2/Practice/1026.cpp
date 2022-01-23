3c20220b5c00be3ae7763f1f07743d488b7cd98f
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:29:54 +0800

    1026

---
+++ Chapter2/Practice/1026.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    char a;
+    int b;
+    float c;
+    double d;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    cout << a << " " << b << " " << fixed << setprecision(6) << c << " " << d << endl;
+    return 0;
+}
