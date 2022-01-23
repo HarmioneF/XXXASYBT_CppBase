61c0f1cc1de184c0d2d76862b9d5c7d1912b0b25
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:43:45 +0800

    1035

---
+++ Chapter2/Practice/1035.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a1, a2, n;
+
+    cin >> a1 >> a2 >> n;
+    int gc = a2 - a1;           // 公差
+    int xz = a1 + (n - 1) * gc;         // 等差数列求项值：首项 + (项数 - 1) * 公差
+
+    cout << xz << endl;
+    return 0;
+}
