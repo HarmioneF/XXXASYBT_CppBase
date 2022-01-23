96bce76ed0b6bbb869dbec6effacc040efee3d22
HHrF <codesiliconxu@gmail.com>
2022-01-23 20:49:41 +0800

    2065

---
+++ Chapter2/Example/2065.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, s;         // int定义a, b, c, s为整形变量
+    cin >> a >> b >> c;     // 输入给定3个整数的值
+    s = a + b + c;          // 求3个整数的和
+    cout << s << endl;      // 输出s的值
+    return 0;
+}
