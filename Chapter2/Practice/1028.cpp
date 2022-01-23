fad91ab7031dff9ccf0396b74555fa87adb7ea25
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:31:30 +0800

    1028

---
+++ Chapter2/Practice/1028.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char x;
+    cin >> x;
+
+    cout << "  " << x << endl;
+    cout << " " << x << x << x << endl;
+    cout << x << x << x << x << x << endl;
+    cout << " " << x << x << x << endl;
+    cout << "  " << x << endl;
+
+    return 0;
+}
