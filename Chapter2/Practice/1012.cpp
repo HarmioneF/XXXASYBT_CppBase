c72e53448dcf6c74f2c32b612f1bb744fc81ef04
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:08:00 +0800

    1012

---
+++ Chapter2/Practice/1012.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    double x, a, b, c, d, e;
+    cin >> x >> a >> b >> c >> d;
+    e = a * pow(x, 3) + b * pow(x, 2) + c * x + d;
+
+    cout << fixed << setprecision(7) << e << endl;
+    return 0;
+}
