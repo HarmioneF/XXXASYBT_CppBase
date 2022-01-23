601a48c0ce417b7ef202f0145fab4d977c010ad4
HHrF <codesiliconxu@gmail.com>
2022-01-23 20:22:15 +0800

    1004

---
+++ Chapter1/Example/1004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char symbol;
+    cin >> symbol;
+    cout << "  " << symbol << endl;
+    cout << " " << symbol << symbol << symbol << endl;
+    cout << symbol << symbol << symbol << symbol << symbol;
+    return 0;
+}
