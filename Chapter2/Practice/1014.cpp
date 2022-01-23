5263c306ab0afeceb3439a11d3fa3cd1a585b907
HHrF <codesiliconxu@gmail.com>
2022-01-23 21:14:56 +0800

    1014

---
+++ Chapter2/Practice/1014.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+#define PI 3.14159
+
+using namespace std;
+
+int main() {
+    double r, d, c, s;
+
+    cin >> r;
+    d = 2 * r;
+    c = 2 * PI * r;
+    s = PI * pow(r, 2);
+
+    cout << fixed << setprecision(4) << d << " " << c << " " << s << endl;
+    return 0;
+}
