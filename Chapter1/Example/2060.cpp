edcc2e95968fe30a7ec8d7d56a2cdd4ed3d22d28
Tom <codesiliconxu@gmail.com>
2022-01-23 19:46:50 +0800

    2060

---
+++ Chapter1/Example/2060.cpp
@@ -0,0 +1,7 @@
+#include<iostream>              // 使用cin,cout，需调用iostream库，否则编译出错
+
+using namespace std;            // 在C语言中要省略，例如在VC++和TC++中
+int main() {                     // 有的C语言可以使用void main(),例如TC++和VC++
+    cout << "Hello World!" << endl;     // 输出"Hello World!"
+    return 0;                   // 结束整个程序，在TC++和VC++中也要保留
+}
