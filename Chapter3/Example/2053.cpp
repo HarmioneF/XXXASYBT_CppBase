ded6956bb6f04fb7397006cda5fe521da8b00e86
HHrF <codesiliconxu@gmail.com>
2022-01-24 16:48:47 +0800

    2053

---
+++ Chapter3/Example/2053.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a, b, c, temp;
+
+    cin >> a >> b >> c;
+    if (a < b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a < c) {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b < c) {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
