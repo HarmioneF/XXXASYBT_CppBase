b156ea01e477706e7d038c47416c31a9950f1bec
HHrF <codesiliconxu@gmail.com>
2022-01-24 16:49:39 +0800

    2054

---
+++ Chapter3/Example/2054.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned t;
+    cin >> t;
+
+    if ((25 <= t) && (t <= 30)){
+        cout << "ok!" << endl;
+    } else cout << "no!" << endl;
+
+    return 0;
+}
