da172b92eb3dba06aa61c1a1efc08981b266794e
HHrF <codesiliconxu@gmail.com>
2022-01-24 16:58:20 +0800

    1048

---
+++ Chapter3/Practice/1048.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int c, m;
+    cin >> c >> m;
+
+    if ((c >= 60) && (m >= 60) or (c <= 60) && (m <= 60))
+        cout << "0" << endl;
+    else
+        cout << "1" << endl;
+
+    return 0;
+}
