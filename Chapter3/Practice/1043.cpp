b6a9565594c438d78c1ff18da822e6952577a626
HHrF <codesiliconxu@gmail.com>
2022-01-24 16:55:13 +0800

    1043

---
+++ Chapter3/Practice/1043.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cin >> x >> y;
+    if (x > y)
+        cout << ">" << endl;
+    else if (x == y)
+        cout << "=" << endl;
+    else
+        cout << "<" << endl;
+
+    return 0;
+}
