9611f4d93a8560ce8d8b510af4e87feeeaba837e
HHrF <codesiliconxu@gmail.com>
2022-01-24 17:30:46 +0800

    Project Fixed.

--- Chapter2/Practice/1016.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int x;
-    short y;
-
-    std::cout << sizeof(x) << " " << sizeof(y) << std::endl;
-    return 0;
-}
--- Chapter2/Practice/1017.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    float x;
-    double y;
-
-    std::cout << sizeof(x) << " " << sizeof(y) << std::endl;
-    return 0;
-}
