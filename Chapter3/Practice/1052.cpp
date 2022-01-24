26e6fbfb1a8b659381b3747fd0a189d998110e9b
HHrF <codesiliconxu@gmail.com>
2022-01-24 17:06:31 +0800

    1052

---
+++ Chapter3/Practice/1052.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int w;
+    int p = 8;
+    char c;
+    cin >> w >> c;
+    switch (w > 1000)
+        case 1:
+            p += int(ceil((w - 1000) / 500.0)) * 4;
+    switch (c)
+        case 'y':
+            p += 5;
+    cout << p;
+    return 0;
+}
