d71e4106a30fc64887b6c26d62aee3f0452b6905
HHrF <codesiliconxu@gmail.com>
2022-01-24 16:55:42 +0800

    1044

---
+++ Chapter3/Practice/1044.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cin >> num;
+    if ((num >= 10) and (num <= 99))
+        cout << "1" << endl;
+    else
+        cout << "0" << endl;
+
+    return 0;
+}
