bc0ed648939cba4c1819681389431d99464d3431
HHrF <codesiliconxu@gmail.com>
2022-01-24 17:14:32 +0800

    2019

---
+++ Chapter3/Practice/2019.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, sum = 1;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        sum *= i;
+    }
+
+    cout << sum << endl;
+    return 0;
+}
