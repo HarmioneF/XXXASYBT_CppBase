5ab1b02200363461b96219f9afa1e95e37241aaf
HHrF.Quiet <codesiliconxu@gmail.com>
2022-02-08 10:33:29 +0800

    1116--Fix Wrong Answer.

--- Chapter5/Practice/1116.cpp
+++ Chapter5/Practice/1116.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
-int a[10001], n, i, k = 1, maxn = 0;
+int n, sum = 1, a[400001], maxn;
 
 int main() {
-    cin >> n;
-    a[0] = -34567;              // a[0]不能和a[1]相等
-    for (i = 1; i <= n; ++i) {
-        cin >> a[i];
-        if (a[i] == a[i - 1])       // 和前一个相等，则平台长度加一
-            ++k;
-        else                        // 不相等， 开始计算下一个平台
-            k = 1;
-        if (k > maxn)               // 找最长平台
-            maxn = k;
-    }
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
 
-    cout << maxn << endl;
+    int temp = a[1];
+    for (int i = 2; i <= n; i++)
+        if (a[i] == temp)
+            sum++;
+        else {
+            maxn = max(maxn, sum);
+            sum = 1;
+            temp = a[i + 1];
+        }
+    maxn = max(maxn, sum);
+
+    printf("%d", maxn);
     return 0;
 }
