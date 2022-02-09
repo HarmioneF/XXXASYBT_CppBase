2678d60bb9d8d6d4a25839d3158493f1491eaf3b
HHrF.Quiet <codesiliconxu@gmail.com>
2022-02-09 11:27:25 +0800

    L5.10

---
+++ Chapter5/Example/L5.10.cpp
@@ -0,0 +1,27 @@
+/* 原书例5.10 */
+#include <iostream>
+#include <iomanip>
+
+const int n = 3, m = 5;
+using namespace std;
+
+int main() {
+    int a[n + 1][m + 1], b[101][4], k = 0;
+    for (int i = 1; i <= n; ++i)             // 矩阵初始
+        for (int j = 1; j <= m; ++j)
+            cin >> a[i][j];
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+            if (a[i][j] != 0) {             // 找到非0值，存储
+                ++k;
+                b[k][1] = i;
+                b[k][2] = j;
+                b[k][3] = a[i][j];
+            }
+    for(int i = 1; i <= k; ++i) {
+        for (int j = 1; j <= 3; ++j)
+            cout << setw(3) << b[i][j];
+        cout << endl;
+    }
+    return 0;
+}
