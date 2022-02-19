a7f782ff892b97046aace178f3185dc77af86c47
Harmione <harmionef@163.com>
2022-02-19 17:48:51 +0800

    L8.1

---
+++ Chapter8/L8.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a, b, s, t, *pa, *pb;
+    pa = &a;
+    pb = &b;
+    a = 10;
+    b = 20;
+    s = *pa + *pb;
+    t = *pa * *pb;
+    printf("a = %d, b = %d\n", *pa, *pb);
+    printf("s = %d, t = %d\n", s, t);
+    return 0;
+}
